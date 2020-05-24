12e7369094b13915f8ba48fdcfd5afd9c3dcabe8
balasaalin <balasaalin@gmail.com>
2020-05-24 23:50:34 +0300

    no message

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -8,10 +8,8 @@ USERNAME=`basename $PARENTDIR`
 
 # Writes the license key in a file
 # Assumes that $LICENSE_KEY contains the license key with all /n's replaced by @@
-echo $LICENSE_KEY
 printf "%s" "$LICENSE_KEY" > tools/oxygen-publishing-engine-3.x/licensekey.txt
 sed -i 's/@@/\n/g' tools/oxygen-publishing-engine-3.x/licensekey.txt
-cat tools/oxygen-publishing-engine-3.x/licensekey.txt
 
 
 # Send some parameters to the "editlink" plugin as system properties
@@ -20,4 +18,4 @@ export ANT_OPTS="$ANT_OPTS -Dcwd=`pwd`"
 export ANT_OPTS="$ANT_OPTS -Drepo.url=github://getFileContent/$USERNAME/$REPONAME/$TRAVIS_BRANCH/"
 export ANT_OPTS="$ANT_OPTS -Dwebhelp.publishing.template.descriptor=$OPT_FILE -Dwebhelp.publishing.template=$OPT_DIR"
 
-tools/oxygen-publishing-engine-3.x/bin/dita -i $DITAMAP -f webhelp-responsive -o $OUT_DIR -t $TEMP_DIR
+tools/oxygen-publishing-engine-3.x/bin/dita -i $DITAMAP -f webhelp-responsive -o $OUT_DIR -t $TEMP_DIR --verbose
