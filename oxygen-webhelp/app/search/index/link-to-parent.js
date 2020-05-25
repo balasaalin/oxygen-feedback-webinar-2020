0dcbef17b83d66e03e700c60ccbe5e9e7c16f223
oXygen XML Deployer <support@oxygenxml.com>
2020-05-25 21:09:30 +0000

    rebuild pages at f460603

--- oxygen-webhelp/app/search/index/link-to-parent.js
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {1:0,13:-1,5:0,7:0,2:0,6:0,4:0,9:0,8:0,3:0,11:0,0:-1,12:-1,10:0};
+return {0:-1,2:-1,1:-1};
 });
